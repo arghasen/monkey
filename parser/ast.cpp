9d5faae60414a1f31301cd6cdfae8414fb36a31f
Argha Sen <iamslowdeath@gmail.com>
2023-06-02 18:34:34 +0800

    chore: To string methods for AST

--- parser/ast.cpp
+++ parser/ast.cpp
@@ -19,6 +19,40 @@ LetStatement::LetStatement(lexer::Token tok) : token(tok) {}
 Identifier::Identifier(lexer::Token tok) : token(tok), value(tok.literal) {}
 ReturnStatement::ReturnStatement(lexer::Token tok) : token(tok) {}
 
+std::string Program::to_string() const {
+  std::string out;
+  for (const auto &stmt : statements) {
+    out += stmt->to_string();
+  }
+  return out;
+}
+
+std::string Identifier::to_string() const { return value; }
+std::string LetStatement::to_string() const {
+  std::string out = token.literal + " " + name->to_string() + " = ";
+  if (value) {
+    out += value->to_string();
+  }
+  out += ";";
+  return out;
+}
+
+std::string ReturnStatement::to_string() const {
+  std::string out = token.literal + " ";
+  if (returnValue) {
+    out += returnValue->to_string();
+  }
+  out += ";";
+  return out;
+}
+
+std::string ExpressionStatement::to_string() const {
+  if (expression) {
+    return expression->to_string();
+  }
+  return "";
+}
+
 } // namespace ast
 } // namespace parser
 } // namespace monkey
