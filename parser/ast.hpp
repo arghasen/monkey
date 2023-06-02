853206a507b5f025207fb918e870aa8e08cc4dc8
Argha Sen <iamslowdeath@gmail.com>
2023-06-03 01:50:28 +0800

    refactor: cleanup token literal and to_string

--- parser/ast.hpp
+++ parser/ast.hpp
@@ -14,28 +14,36 @@ public:
   virtual ~Node() = default;
   virtual std::string to_string() const = 0;
   virtual std::string TokenLiteral() const = 0;
+protected:
+    Node() = default;
 };
 
 class Statement : public Node {
 public:
   virtual ~Statement() = default;
   virtual std::string to_string() const = 0;
-  virtual std::string TokenLiteral() const = 0;
+  std::string TokenLiteral() const override;
+  lexer::Token token;
+protected:
+    explicit Statement(lexer::Token tok) ;
 };
 
 class Expression : public Node {
 public:
   virtual ~Expression() = default;
-  virtual std::string to_string() const = 0;
-  virtual std::string TokenLiteral() const = 0;
+  std::string to_string() const override;
+  std::string TokenLiteral() const override;
+  lexer::Token token;
+protected:
+    explicit Expression(lexer::Token tok) ;
 };
 
 class Program : public Node {
 public:
   Program() = default;
   ~Program() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
+  std::string TokenLiteral() const override;
   std::vector<std::unique_ptr<Statement>> statements;
 };
 
@@ -44,32 +52,24 @@ public:
   Identifier() = default;
   explicit Identifier(lexer::Token tok);
   ~Identifier() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::string value;
 };
 
 class LetStatement : public Statement {
 public:
-  LetStatement() = default;
   explicit LetStatement(lexer::Token tok);
   ~LetStatement() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::unique_ptr<Identifier> name;
   std::unique_ptr<Expression> value;
 };
 
 class ReturnStatement : public Statement {
 public:
-  ReturnStatement() = default;
   explicit ReturnStatement(lexer::Token tok);
   ~ReturnStatement() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::unique_ptr<Expression> returnValue;
 };
 
@@ -78,46 +78,41 @@ public:
   ExpressionStatement() = default;
   explicit ExpressionStatement(lexer::Token tok);
   ~ExpressionStatement() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::unique_ptr<Expression> expression;
 };
 
 class IntegerLiteral : public Expression {
 public:
-  IntegerLiteral() = default;
   explicit IntegerLiteral(lexer::Token tok);
   ~IntegerLiteral() override = default;
-  std::string TokenLiteral() const override;
-  std::string to_string() const override;
-  lexer::Token token;
   int64_t value;
 };
 
 class PrefixExpression : public Expression {
 public:
-  PrefixExpression() = default;
-  PrefixExpression(lexer::Token tok);
+  explicit PrefixExpression(lexer::Token tok);
   ~PrefixExpression() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::string op;
   std::unique_ptr<Expression> right;
 };
 
 class InfixExpression : public Expression {
 public:
-  InfixExpression() = default;
-  InfixExpression(lexer::Token tok);
+  explicit InfixExpression(lexer::Token tok);
   ~InfixExpression() override = default;
-  std::string TokenLiteral() const override;
   std::string to_string() const override;
-  lexer::Token token;
   std::unique_ptr<Expression> left;
   std::string op;
   std::unique_ptr<Expression> right;
+};
+
+class Boolean : public Expression {
+public:
+  explicit Boolean(lexer::Token tok);
+  ~Boolean() override = default;
+  bool value;
 };
 
 template<typename Y>
