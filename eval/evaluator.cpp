2cb86e020bf8f3efcf4b157d98ed96e062a19bc6
Argha Sen <iamslowdeath@gmail.com>
2023-06-03 16:52:39 +0800

    chore: groundwork for prasing expressions

--- eval/evaluator.cpp
+++ eval/evaluator.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 namespace monkey::evaluator{
 
+const static auto TRUE = std::make_shared<Boolean>(true);
+const static auto FALSE = std::make_shared<Boolean>(false);
+const static auto NullObject = std::make_shared<Null>();
+
 ObjectPtr Evaluator::doEval(const parser::ast::Statements& node){
     std::cout<<"evaluating statements"<<std::endl;
-    std::unique_ptr<Object> result;
+    std::shared_ptr<Object> result;
     for(auto& stmt : node){
         result = eval(stmt.get());
     }
@@ -19,15 +23,71 @@ ObjectPtr Evaluator::doEval(parser::ast::Statement* node) {
     return nullptr;
 }
 
+ObjectPtr Evaluator::doEval(parser::ast::IntegerLiteral* node){
+    return std::make_shared<Integer>(node->value);
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::Boolean* node){
+    return (node->value) ? TRUE : FALSE;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::InfixExpression* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::PrefixExpression* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::IfExpression* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::FunctionLiteral* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::CallExpression* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::Identifier* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::LetStatement* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::ReturnStatement* node){
+    return nullptr;
+}
+
+ObjectPtr Evaluator::doEval(parser::ast::BlockStatement* node){
+    return nullptr;
+}
+
 ObjectPtr Evaluator::doEval(parser::ast::Expression* node) {
     std::cout<<"evaluating expression"<< (int)node->Type()<<std::endl;
-    if (node->Type() == parser::ast::ExpressionType::INTEGER)
-    {
-        return std::make_unique<Integer>(static_cast<parser::ast::IntegerLiteral*>(node)->value);
-    }
-    else if (node->Type() == parser::ast::ExpressionType::BOOLEAN)
-    {
-        return std::make_unique<Boolean>(static_cast<parser::ast::Boolean*>(node)->value);
+    switch(node->Type()){
+        case parser::ast::ExpressionType::IDENTIFIER:
+            return doEval(static_cast<parser::ast::Identifier*>(node));
+        case parser::ast::ExpressionType::INTEGER:
+            return doEval(static_cast<parser::ast::IntegerLiteral*>(node));
+        case parser::ast::ExpressionType::BOOLEAN:
+            return doEval(static_cast<parser::ast::Boolean*>(node));
+        case parser::ast::ExpressionType::PREFIX:
+            return doEval(static_cast<parser::ast::PrefixExpression*>(node));
+        case parser::ast::ExpressionType::INFIX:
+            return doEval(static_cast<parser::ast::InfixExpression*>(node));
+        case parser::ast::ExpressionType::IF:
+            return doEval(static_cast<parser::ast::IfExpression*>(node));
+        case parser::ast::ExpressionType::FUNCTION:
+            return doEval(static_cast<parser::ast::FunctionLiteral*>(node));
+        case parser::ast::ExpressionType::CALL:
+            return doEval(static_cast<parser::ast::CallExpression*>(node));
+        default:
+            return nullptr;
     }
     return nullptr;
 }
